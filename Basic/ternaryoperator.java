fdd57371905f55dd8b13dc66a6d4fb2eb05c5f07
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2025-11-22 09:43:20 +0530

    added new file

---
+++ Basic/ternaryoperator.java
@@ -0,0 +1,10 @@
+package Basic;
+
+public class ternaryoperator {
+    public static void main (String args[])
+    {
+        int n=4;
+        String result = (n%2==0)? "Even number" : "odd number";
+        System.out.println(result);
+    }
+}
