0efd2cfd8d2aad103a8393b5ad023e18d5121503
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2025-11-22 09:44:23 +0530

    added new file

--- Basic/ternaryoperator.java
+++ Basic/ternaryoperator.java
@@ -1,3 +1,9 @@
+/* A ternary operator example 
+it works as :- condition ? expression1 : expression2
+where condition gets true the expression1 will store in resutl else expression2 will store in result
+like in below exampel if n%2==0 is true then "Even number" will store in result else "odd number" will store in result
+*/
+
 package Basic;
 
 public class ternaryoperator {
