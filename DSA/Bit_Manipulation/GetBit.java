be9426b2fb394714299283355c67ec4fff68c220
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-01 12:07:16 +0530

    added BitManupulation

---
+++ DSA/Bit_Manipulation/GetBit.java
@@ -0,0 +1,19 @@
+class GetBit{
+
+  public static String getBit(int binaryValue,int pos){
+    int bitMask=1<<pos;
+    if((binaryValue & bitMask)!=0){
+      return "Bit is one";
+    } 
+    else{
+      return "Bit is zero";
+      }
+  }
+  public static void main (String args[]){
+  
+    // Use 0b to tell Java this is binary
+    int binaryValue=0b0101;
+    int pos=2;
+    System.out.println(getBit(binaryValue,pos));    
+  }
+}
