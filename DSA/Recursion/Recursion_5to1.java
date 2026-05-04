7e58d848a5d2f8c92d69146d601220a791aa143b
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-04 09:46:37 +0530

    added recursion

---
+++ DSA/Recursion/Recursion_5to1.java
@@ -0,0 +1,11 @@
+class Recursion_5to1{
+  public static void printNumber(int n){
+    if(n==0) return;
+    System.out.print(n+" ");
+    printNumber(n-1);
+  }
+
+  public static void main (String args[]){
+    printNumber(5);
+  }
+}
