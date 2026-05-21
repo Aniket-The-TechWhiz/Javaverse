ebaa69b98fd13cb06d0d33aeff6d48eb5c3d5f19
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-21 17:00:47 +0530

    change the standard code and added the sort fucntion for the arraylist

--- practice/Arraylistclass.java
+++ practice/Arraylistclass.java
@@ -11,11 +11,17 @@ public class Arraylistclass {
         // Collections.sort(arr);
         // System.out.println(arr);
 
-        ArrayList<String> arr=new ArrayList<>(Arrays.asList("Banana", "Apple", "Orange"));
+        // ArrayList<String> arr=new ArrayList<>(Arrays.asList("Banana", "Apple", "Orange"));
         
-        List<String> sortedList = arr.stream()
-                                .sorted()
-                                .collect(Collectors.toList());
+        // List<String> sortedList = arr.stream()
+        //                         .sorted()
+        //                         .collect(Collectors.toList());
+        // System.out.println(sortedList);
+
+        ArrayList<Integer> arr=new ArrayList<>(Arrays.asList(2,1,3,4,5,6));
+        List<Integer> sortedList = arr.stream()
+                                    .filter(x->x%2==0)
+                                    .toList();
         System.out.println(sortedList);
     }
 }
