85e6be39d8218b39a3650c1c6cbacdbfd02bb99b
Aniket-The-TechWhiz <aniketyelameli@gmail.com>
2026-05-21 19:57:59 +0530

    added new file

--- practice/Hashsetclass.java
+++ practice/Hashsetclass.java
@@ -1,13 +1,15 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 public class Hashsetclass{
   public static void main(String args[])
   {
-    HashSet <Integer> hs= new HashSet<Integer>();
-    hs.add(1);
-    hs.add(2);
-    hs.add(3);
-    hs.add(1);
-    System.out.println(hs);
+    HashSet<Integer> hs = new HashSet<>(Arrays.asList(1, 2, 3, 1));
+    List<Integer> filteredList = hs.stream()
+                               .filter(x -> x % 2 == 0)
+                               .toList();
+
+    System.out.println(filteredList);
   }
 }
